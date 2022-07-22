854a79fdc66d8fbc726313b1e4941ae54332976f
Юрий Хоружий <urijhoruzij@gmail.com>
2022-07-22 11:38:22 +0300

    feat: add Avatar

---
+++ src/components/Avatar/Avatar.stories.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Avatar } from "../";
+
+export default {
+  title: "Components/Avatar",
+  component: Avatar,
+};
+
+export const Small = () => (
+  <Avatar
+    avatar="https://images.unsplash.com/photo-1517841905240-472988babdf9"
+    fullname="Helena Lopes"
+    isOnline
+    size="small"
+  />
+);
+export const Medium = () => (
+  <Avatar
+    avatar="https://images.unsplash.com/photo-1517841905240-472988babdf9"
+    fullname="Helena Lopes"
+    isOnline
+    size="medium"
+  />
+);
+export const Large = () => (
+  <Avatar
+    avatar="https://images.unsplash.com/photo-1517841905240-472988babdf9"
+    fullname="Helena Lopes"
+    isOnline
+    size="large"
+  />
+);
+export const SymbolSmall = () => (
+  <Avatar fullname="Helena Lopes" isOnline size="small" />
+);
+export const SymbolMedium = () => (
+  <Avatar fullname="Helena Lopes" isOnline size="medium" />
+);
+export const SymbolLarge = () => (
+  <Avatar fullname="Helena Lopes" isOnline size="large" />
+);
+const img = (props) => (
+  <img
+    alt=""
+    src="https://images.unsplash.com/photo-1517841905240-472988babdf9"
+    {...props}
+  />
+);
+export const ComponentSmall = () => (
+  <Avatar component={img} fullname="Helena Lopes" isOnline size="small" />
+);
+export const ComponentMedium = () => (
+  <Avatar component={img} fullname="Helena Lopes" isOnline size="medium" />
+);
+export const ComponentLarge = () => (
+  <Avatar component={img} fullname="Helena Lopes" isOnline size="large" />
+);
