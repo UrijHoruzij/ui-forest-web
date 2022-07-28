405861643487769e8cc9fc8ddee3e6d8c0b016fe
Юрий Хоружий <urijhoruzij@gmail.com>
2022-07-28 14:43:36 +0300

    fix: remove * selector from GlobalStyle

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,27 +1,27 @@
-import light from "../src/components/ThemeProvider/themes/light.module.css";
-import dark from "../src/components/ThemeProvider/themes/dark.module.css";
+import light from '../src/components/ThemeProvider/themes/light.module.css';
+import dark from '../src/components/ThemeProvider/themes/dark.module.css';
 
 export const decorators = [
-  (Story) => (
-    <>
-      {/* <GlobalStyle /> */}
-      <Story />
-    </>
-  ),
+	(Story) => (
+		<>
+			<GlobalStyle />
+			<Story />
+		</>
+	),
 ];
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  themes: {
-    default: "light",
-    list: [
-      { name: "light", class: [light.root], color: "#f5f5f5" },
-      { name: "dark", class: [dark.root], color: "#333333" },
-    ],
-  },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
+	actions: { argTypesRegex: '^on[A-Z].*' },
+	themes: {
+		default: 'light',
+		list: [
+			{ name: 'light', class: [light.root], color: '#f5f5f5' },
+			{ name: 'dark', class: [dark.root], color: '#333333' },
+		],
+	},
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/,
+		},
+	},
 };
