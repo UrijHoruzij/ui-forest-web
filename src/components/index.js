5c1da018d5a696fe4eefd740f4741d6eaa7f70d7
Юрий Хоружий <urijhoruzij@gmail.com>
2022-08-02 10:28:48 +0300

    fix: remove Markdown

--- src/components/index.js
+++ src/components/index.js
@@ -10,7 +10,7 @@ export { default as NotificationProvider } from './Notification';
 export { default as useNotification } from './Notification/useNotification';
 export { default as Grid } from './Grid';
 export { default as Button } from './Button';
-export { default as Markdown } from './Markdown';
+// export { default as Markdown } from './Markdown';
 
 // export { default as ZoomImage } from './ZoomImage';
 // export { default as Skeleton } from './Skeleton';
